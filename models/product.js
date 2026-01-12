44fb3f3677ea32786bb33e5fe189f5c92c4db941
FallenMAD <naruto.yzymaki4@gmail.com>
2026-01-12 20:45:02 +0100

    feature(product): create new product model

--- models/product.js
+++ models/product.js
@@ -1,26 +1,59 @@
-import { DataTypes } from 'sequelize';
-import { sequelize } from '../utils/database.js';
+import mongoDB from 'mongodb';
+import { getDb } from '../utils/database.js';
 
-export const Product = sequelize.define('Product', {
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  title: {
-    type: DataTypes.STRING,
-  },
-  price: {
-    type: DataTypes.DOUBLE,
-    allowNull: false,
-  },
-  imageURL: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+export class Product {
+  constructor(title, price, description, imageURL, id) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageURL = imageURL;
+    this._id = id;
+  }
+
+  save() {
+    let dbOp;
+    const db = getDb();
+    if (this._id) {
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: new mongoDB.ObjectId(this._id) }, { $set: this });
+    } else {
+      dbOp = db
+        .collection('products')
+        .insertOne(this)
+    }
+    return dbOp
+      .then(record => {
+        console.log(record)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db.collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+        // console.log(products);
+        return products;
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  }
+
+  static findOne(id) {
+    const db = getDb();
+    return db.collection('products')
+      .findOne({ _id: new mongoDB.ObjectId(id) })
+      .then(product => {
+        return product;
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+}
