fa95ccd4abe801b899a2f935e967a83ae5f99f88
FallenMAD <naruto.yzymaki4@gmail.com>
2026-01-12 20:43:10 +0100

    feature(database): create connection to db

--- utils/database.js
+++ utils/database.js
@@ -1,14 +1,36 @@
-import { Sequelize } from 'sequelize';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-  `${process.env.DB_NAME}`,
-  `${process.env.DB_USER}`,
-  `${process.env.DB_PASSWORD}`,
-  {
-    dialect: 'mysql',
-    host: 'localhost',
+const uri = `mongodb+srv://roman:${process.env.DB_PASSWORD}@cluster0.sdvrrdi.mongodb.net/?retryWrites=true&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let db;
+
+export async function connectToDatabase() {
+  try {
+    await client.connect();
+    await client.db('admin').command({ ping: 1 });
+    console.log('Connected to MongoDB Atlas!');
+
+    db = client.db('shop'); // <-- назва твоєї бази
+  } catch (err) {
+    console.error('❌ Error connecting to MongoDB:', err);
+    process.exit(1);
   }
-);
+}
+
+export function getDb() {
+  if (!db) {
+    throw new Error('❌ Database not initialized!');
+  }
+  return db;
+}
