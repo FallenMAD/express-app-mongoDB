337af0e467680e06645465208e4b64147a83ebec
FallenMAD <naruto.yzymaki4@gmail.com>
2026-01-12 20:44:01 +0100

    refactor(app): use mongodb and new start server fn

--- app.js
+++ app.js
@@ -6,14 +6,7 @@ import { rootDir } from './utils/dirnameHelper.js';
 import adminRoutes from './routes/admin.js';
 import shopRoutes from './routes/shop.js';
 import { errorController } from './controllers/error.js';
-import { sequelize } from './utils/database.js';
-
-import { Product } from './models/product.js';
-import { User } from './models/user.js';
-import { Cart } from './models/cart.js';
-import { CartItem } from './models/cart-item.js';
-import { Order } from './models/order.js';
-import { OrderItem } from './models/order-item.js';
+import { connectToDatabase } from './utils/database.js';
 
 const app = express();
 
@@ -23,56 +16,17 @@ app.set('views', 'views');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.handle404);
 
-Product.belongsTo(User, {
-  constraints: true,
-  onDelete: 'CASCADE',
-});
-User.hasMany(Product);
+async function startServer() {
+  await connectToDatabase();
 
-User.hasOne(Cart);
-Cart.belongsTo(User);
-
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-sequelize
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      console.log('User not found');
-      return User.create({ name: 'Roman', email: 'test@example.com' });
-    }
-    return Promise.resolve(user);
-  })
-  .then((user) => {
-    return user.createCart();
-  })
-  .then(() => {
-    app.listen(3003);
-  })
-  .catch((error) => {
-    console.log(error);
+  app.listen(3003, () => {
+    console.log('Server started on port 3000');
   });
+}
+
+startServer();
