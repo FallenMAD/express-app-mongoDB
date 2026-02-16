39ab18a776ced14886c6eaeb1966e93c5c5b96da
FallenMAD <naruto.yzymaki4@gmail.com>
2026-02-16 20:22:01 +0100

    feature(app): add user

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ import path from 'path';
 import express from 'express';
 
 import { rootDir } from './utils/dirnameHelper.js';
+import { connectToDatabase } from './utils/database.js';
 
 import adminRoutes from './routes/admin.js';
 import shopRoutes from './routes/shop.js';
 import { errorController } from './controllers/error.js';
-import { connectToDatabase } from './utils/database.js';
+
+import { User } from './models/user.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
@@ -15,6 +20,15 @@ app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
+
+app.use((req, res, next) => {
+  User.findById(process.env.USER_ID)
+    .then((user) => {
+      req.user = new User(user.username, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => {});
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -25,7 +39,7 @@ async function startServer() {
   await connectToDatabase();
 
   app.listen(3003, () => {
-    console.log('Server started on port 3000');
+    console.log('Server started on port 3003');
   });
 }
 
